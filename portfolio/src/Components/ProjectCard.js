1ab2da46f323f17db243cbef1adc98d2d4c3a5bd
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2023-08-07 23:31:59 +0530

    Project section
    Made a separate card for project section and designed it a bit

--- portfolio/src/Components/ProjectCard.js
+++ portfolio/src/Components/ProjectCard.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import { Card, CardBody, CardFooter,Button,ButtonGroup,Image,Stack,Heading,Text} from '@chakra-ui/react'
+import { Card, CardBody, CardFooter,Button,ButtonGroup,Image,Stack,Heading,Text,Box,Flex} from '@chakra-ui/react'
 import 'aos/dist/aos.css'
 
 export default function ProjectCard(props) {
   return (
-    <div data-aos="zoom-in" data-aos-duration="700" data-aos-delay={props.delay}>
-  <Card maxW='xs' mb="5" borderRadius="xl" m="5">
-  <CardBody background="linear-gradient(135deg, white, #E0F8FF);" borderRadius="xl">
+  <div data-aos="zoom-in" data-aos-duration="700" data-aos-delay={props.delay}>
+  {/* <Card maxW='sm' mb="5" borderRadius="xl" m="5" background="rgba(255, 255, 255, 0)" _hover={{ background: 'rgba(255, 255, 255, 0.2)' }} border="0px">
+  <CardBody  borderRadius="xl">
     <Image 
       border="green solid 2px"
       src={props.img}
       alt='Green double couch with wooden legs'
       borderRadius='xl'
     />
+    
     <Stack  spacing='2'>
-      <Heading size='md'>E-commerce website</Heading>
-      <Text>
+      <Heading size='sm' color="#4abca8">E-commerce website</Heading>
+      <Text color="white" fontSize="xs">
         This sofa is perfect for modern tropical spaces, baroque inspired
         spaces, earthy toned spaces and for people who love a chic design with a
         sprinkle of vintage design.
@@ -25,11 +26,25 @@ export default function ProjectCard(props) {
     </Button>
     </Stack>
   </CardBody>
-  {/* <Divider /> */}
-  {/* <CardFooter> */}
-    
-  {/* </CardFooter> */}
-</Card>
+ 
+</Card> */}
+<Flex m="5" _hover={{ background: 'rgba(255, 255, 255, 0.2)' }} p="5" borderRadius="lg">
+  <Box>
+  <Image 
+      maxW="md"
+      border="green solid 2px"
+      src={props.img}
+      alt='Green double couch with wooden legs'
+      borderRadius='xl'
+    />
+  </Box>
+  <Box m="3">
+    <Heading size='md' color="#4abca8">{props.title}</Heading>
+    <Text color="white" fontSize="sm">
+          {props.desc}
+    </Text>
+  </Box>
+</Flex>
 </div>
   )
 }
--- portfolio/src/Pages/Project.js
+++ portfolio/src/Pages/Project.js
@@ -2,13 +2,18 @@ import { Flex ,Text} from '@chakra-ui/react'
 import React from 'react'
 import Card from '../Components/ProjectCard'
 export default function Project() {
+  const p1="It is an e-commerce website, designed to showcase the functionality of an online shopping experience. It includes a home page, individual product page, cart feature and a demo payment form along with authorization using jwt token authentication and few other features. Its made using react, mongoose, express, chakra ui and node.js"
+  const p2="It is a simple and user-friendly platform where we can post our goals, set priorities, and track our progress. With just a few clicks, we can define our objectives, determine when to start and finish them, and stay motivated along the way. It is a mern (MongoDB, Express, React,Node) website with jwt token authentication."
+  const p3="This is a website based on a ML model which was trained using health and lifestyle dataset using logistic regression and can predict thechances of sleep disorder. The training of the model was done in jupyter notebook along with data analysis and cleaning. The website is made using react, chakra and flask by importing the same model trained in jupyter notebook using pickle library."
   return (
     <Flex direction="column" alignItems="center" id="projects">
         <Text fontSize="4xl" color="#4abca8" mb="5">Projects</Text>
-        <Flex>
-          <Card delay={100} img={"ecom.png"}/>
-          <Card delay={300} img={"goal.png"}/>
-          {/* <Card delay={600}/> */}
+        <Flex direction="column">
+          <Card delay={100} img={"ecom.png"} title="E-commerce website" desc={p1}/>
+          <Card delay={200} img={"goal.png"} title="Goal Manager" desc={p2}/>
+          <Card delay={300} img={"sleep.jpg"} title="Sleep disorder predictor" desc={p3}/>
+
+        
         </Flex>
        
 
